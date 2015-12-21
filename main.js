1803688e4989514c622bdeebbc6e74461e823ff6
wingillis <win.gillis@gmail.com>
2015-12-20 21:37:02 -0500

    Adding text strings

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // game handling variables
-var platforms, player, cursors
+var platforms, player, cursors, textInstance, keyN
 // flags
-var jumping
+var jumping, textIndex
+
+var text = ['Hello and welcome to our game. Please press "n" to continue',
+            'We are pleased to announce that we have no experience making games',
+            'What you see here is our silly attempt at such a thing',
+            'We won\'t mind if you decide to quit']
 
 // Using https://phaser.io as a gaming platform
 var game = new Phaser.Game(700, 500, Phaser.AUTO, '', {
@@ -24,13 +29,15 @@ function update() {
   } else if (cursors.right.isDown) {
     player.body.velocity.x = 150
   }
-  if (cursors.up.isDown && jumping !== true) {
-    // negative is up
+  if (cursors.up.isDown && player.body.touching.down) {
+    // negative is up is px/s
     player.body.velocity.y = -150
   }
+
 }
 
 function create() {
+  textIndex = 0
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
   platforms = game.add.group()
@@ -45,4 +52,12 @@ function create() {
   player.body.collideWorldBounds = true
   // circle.enableBody = true
   cursors = game.input.keyboard.createCursorKeys()
+  keyN = game.input.keyboard.addKey(Phaser.Keyboard.N)
+  keyN.onDown.add(function(key) {
+    textIndex++
+    if (textIndex < text.length){
+      textInstance.text = text[textIndex]
+    }
+  })
+  textInstance = game.add.text(10,10, text[textIndex], {'fill': 'white', fontSize: '14pt'})
 }
